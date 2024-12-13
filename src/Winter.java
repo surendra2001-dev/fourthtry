1e14dbe1c95442ea7a75fe42f9725b73fedbef33
surendra2001-dev <kunwarsurendra02@gmail.com>
2024-12-13 10:45:36 +0545

    jusr third try

---
+++ src/Winter.java
@@ -0,0 +1,10 @@
+
+public class Winter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java is a funny language but i love it ");
+
+	}
+
+}
